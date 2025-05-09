31d559dae4007f64004356175ca71d8de65732dc
shirleytran04 <im.shirleytran@gmail.com>
2025-05-09 17:39:56 +0700

    Update all pages to tabs

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -13,47 +13,46 @@ export default function TabLayout() {
 
   return (
     <Tabs
-  screenOptions={{
-    tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-    headerShown: false,
-    tabBarButton: HapticTab,
-    tabBarBackground: TabBarBackground,
-    tabBarStyle: Platform.select({
-      ios: {
-        position: "absolute",
-      },
-      default: {},
-    }),
-  }}
->
-  <Tabs.Screen
-    name="index"
-    options={{
-      title: "Home",
-      tabBarIcon: ({ color }) => (
-        <IconSymbol size={28} name="house.fill" color={color} />
-      ),
-    }}
-  />
-  <Tabs.Screen
-    name="explore"
-    options={{
-      title: "Explore",
-      tabBarIcon: ({ color }) => (
-        <IconSymbol size={28} name="paperplane.fill" color={color} />
-      ),
-    }}
-  />
-  <Tabs.Screen
-    name="search"
-    options={{
-      title: "Search",
-      tabBarIcon: ({ color }) => (
-        <IconSymbol size={28} name="magnifyingglass" color={color} />
-      ),
-    }}
-  />
-</Tabs>
-
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="magnifyingglass" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
