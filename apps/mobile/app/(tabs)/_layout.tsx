c990d11273b3a2705f23655a603158f6c74e1b81
shirleytran04 <im.shirleytran@gmail.com>
2025-06-02 22:26:16 +0700

    Rebase this branch onto main

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -8,8 +8,6 @@ import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
-//watch list
-
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
